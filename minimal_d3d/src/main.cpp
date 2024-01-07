aeb50958fa8595137c64e616814e09b15579d7f0
Stone Chen <chen.stonechen@gmail.com>
2024-01-06 19:47:00 -0500

    switch to glm

--- minimal_d3d/src/main.cpp
+++ minimal_d3d/src/main.cpp
@@ -12,11 +12,11 @@
 #include "xdata.h" // 3d model
 
 #include <SDL.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 
 struct float3 { float x, y, z; };
-struct matrix { float m[4][4]; };
-
-matrix operator*(const matrix& m1, const matrix& m2);
 
 SDL_Window* initSDL_Window()
 {
@@ -81,18 +81,18 @@ void CreateDeviceAndSwapchain(IDXGISwapChain1** swapChain, ID3D11Device1** devic
    dxgiAdapter->GetParent(__uuidof(IDXGIFactory2), reinterpret_cast<void**>(&dxgiFactory));
    
    DXGI_SWAP_CHAIN_DESC1 swapChainDesc;
-   swapChainDesc.Width = 0; // use window wCreateDidth
-   swapChainDesc.Height = 0; // use window height
-   swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB;
-   swapChainDesc.Stereo = FALSE;
-   swapChainDesc.SampleDesc.Count = 1;
+   swapChainDesc.Width              = 0; // use window wCreateDidth
+   swapChainDesc.Height             = 0; // use window height
+   swapChainDesc.Format             = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB;
+   swapChainDesc.Stereo             = FALSE;
+   swapChainDesc.SampleDesc.Count   = 1;
    swapChainDesc.SampleDesc.Quality = 0;
-   swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-   swapChainDesc.BufferCount = 2;
-   swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
-   swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD; // prefer DXGI_SWAP_EFFECT_FLIP_DISCARD, see Minimal D3D11 pt2 
-   swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
-   swapChainDesc.Flags = 0;
+   swapChainDesc.BufferUsage        = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+   swapChainDesc.BufferCount        = 2;
+   swapChainDesc.Scaling            = DXGI_SCALING_STRETCH;
+   swapChainDesc.SwapEffect         = DXGI_SWAP_EFFECT_DISCARD; // prefer DXGI_SWAP_EFFECT_FLIP_DISCARD, see Minimal D3D11 pt2 
+   swapChainDesc.AlphaMode          = DXGI_ALPHA_MODE_UNSPECIFIED;
+   swapChainDesc.Flags              = 0;
    
    HWND window = GetActiveWindow(); // maybe move this directly to whwere I need
    dxgiFactory->CreateSwapChainForHwnd(*device, window, &swapChainDesc, nullptr, nullptr, swapChain);
@@ -109,29 +109,36 @@ int main(int argc, char** args)
 
    CreateDeviceAndSwapchain(&swapChain, &device, &deviceContext);
 
-   // from the swap chain, grab the buuffers I want 
+   // from the swap chain, grab the buffers I want 
    ID3D11Texture2D* frameBuffer;
-   swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&frameBuffer));
-   ID3D11RenderTargetView* frameBufferView;
+   swapChain->GetBuffer(0,
+      __uuidof(ID3D11Texture2D),
+      reinterpret_cast<void**>(&frameBuffer));
 
-   // create render view target
+   ID3D11RenderTargetView* frameBufferView;
    device->CreateRenderTargetView(frameBuffer, nullptr, &frameBufferView);
 
    D3D11_TEXTURE2D_DESC depthBufferDesc;
    frameBuffer->GetDesc(&depthBufferDesc); // copy from framebuffer properties
    depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
    depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-
    ID3D11Texture2D* depthBuffer;
    device->CreateTexture2D(&depthBufferDesc, nullptr, &depthBuffer);
-
    ID3D11DepthStencilView* depthBufferView;
    device->CreateDepthStencilView(depthBuffer, nullptr, &depthBufferView);
 
    ID3DBlob* vsBlob;
-   D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "vs_main", "vs_5_0", 0, 0, &vsBlob, nullptr);
+   D3DCompileFromFile(L"shaders.hlsl",
+      nullptr, nullptr,
+      "vs_main", "vs_5_0",
+      0, 0, &vsBlob, nullptr);
    ID3D11VertexShader* vertexShader;
-   device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &vertexShader);
+   device->CreateVertexShader(
+      vsBlob->GetBufferPointer(),
+      vsBlob->GetBufferSize(),
+      nullptr,
+      &vertexShader);
+
    D3D11_INPUT_ELEMENT_DESC inputElementDesc[] = // float3 position, float3 normal, float2 texcoord, float3 color
    {
        { "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,                            0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
@@ -139,13 +146,18 @@ int main(int argc, char** args)
        { "TEX", 0, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
        { "COL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
    };
-   ID3D11InputLayout* inputLayout;
-   device->CreateInputLayout(inputElementDesc, ARRAYSIZE(inputElementDesc), vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &inputLayout);
 
    ID3DBlob* psBlob;
    D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "ps_main", "ps_5_0", 0, 0, &psBlob, nullptr);
    ID3D11PixelShader* pixelShader;
    device->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &pixelShader);
+
+   ID3D11InputLayout* inputLayout;
+   device->CreateInputLayout(inputElementDesc,
+      ARRAYSIZE(inputElementDesc),
+      vsBlob->GetBufferPointer(),
+      vsBlob->GetBufferSize(),
+      &inputLayout);
 
    D3D11_RASTERIZER_DESC1 rasterizerDesc = {};
    rasterizerDesc.FillMode = D3D11_FILL_SOLID;
@@ -154,76 +166,67 @@ int main(int argc, char** args)
    device->CreateRasterizerState1(&rasterizerDesc, &rasterizerState);
 
    D3D11_SAMPLER_DESC samplerDesc = {};
-   samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
-   samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-   samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-   samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+   samplerDesc.Filter         = D3D11_FILTER_MIN_MAG_MIP_POINT;
+   samplerDesc.AddressU       = D3D11_TEXTURE_ADDRESS_WRAP;
+   samplerDesc.AddressV       = D3D11_TEXTURE_ADDRESS_WRAP;
+   samplerDesc.AddressW       = D3D11_TEXTURE_ADDRESS_WRAP;
    samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-
    ID3D11SamplerState* samplerState;
-
    device->CreateSamplerState(&samplerDesc, &samplerState);
 
    D3D11_DEPTH_STENCIL_DESC depthStencilDesc = {};
    depthStencilDesc.DepthEnable = TRUE;
    depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
    depthStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;
-
    ID3D11DepthStencilState* depthStencilState;
-
    device->CreateDepthStencilState(&depthStencilDesc, &depthStencilState);
 
    // constants are uniforms basically?
-
    struct Constants
    {
-      matrix Transform;
-      matrix Projection;
+      glm::mat4 Transform;
+      glm::mat4 Projection;
       float3 LightVector;
    };
 
    D3D11_BUFFER_DESC constantBufferDesc = {};
-   constantBufferDesc.ByteWidth = sizeof(Constants) + 0xf & 0xfffffff0; // round constant buffer size to 16 byte boundary
-   constantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-   constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+   // round constant buffer size to 16 byte boundary
+   constantBufferDesc.ByteWidth      = sizeof(Constants) + 0xf & 0xfffffff0;
+   constantBufferDesc.Usage          = D3D11_USAGE_DYNAMIC;
+   constantBufferDesc.BindFlags      = D3D11_BIND_CONSTANT_BUFFER;
    constantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-
    ID3D11Buffer* constantBuffer;
-
    device->CreateBuffer(&constantBufferDesc, nullptr, &constantBuffer);
 
    D3D11_BUFFER_DESC vertexBufferDesc = {};
-   vertexBufferDesc.ByteWidth = sizeof(VertexData);
-   vertexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
-   vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-
+   vertexBufferDesc.ByteWidth        = sizeof(VertexData);
+   vertexBufferDesc.Usage            = D3D11_USAGE_IMMUTABLE;
+   vertexBufferDesc.BindFlags        = D3D11_BIND_VERTEX_BUFFER;
    D3D11_SUBRESOURCE_DATA vertexData = { VertexData };
    ID3D11Buffer* vertexBuffer;
    device->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer);
 
    D3D11_BUFFER_DESC indexBufferDesc = {};
-   indexBufferDesc.ByteWidth = sizeof(IndexData);
-   indexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
-   indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
-
-   D3D11_SUBRESOURCE_DATA indexData = { IndexData };
+   indexBufferDesc.ByteWidth         = sizeof(IndexData);
+   indexBufferDesc.Usage             = D3D11_USAGE_IMMUTABLE;
+   indexBufferDesc.BindFlags         = D3D11_BIND_INDEX_BUFFER;
+   D3D11_SUBRESOURCE_DATA indexData  = { IndexData };
    ID3D11Buffer* indexBuffer;
    device->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer);
 
    D3D11_TEXTURE2D_DESC textureDesc = {};
-   textureDesc.Width = TEXTURE_WIDTH;  // in xdata.h
-   textureDesc.Height = TEXTURE_HEIGHT; // in xdata.h
-   textureDesc.MipLevels = 1;
-   textureDesc.ArraySize = 1;
-   textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+   textureDesc.Width            = TEXTURE_WIDTH;  // in xdata.h
+   textureDesc.Height           = TEXTURE_HEIGHT; // in xdata.h
+   textureDesc.MipLevels        = 1;
+   textureDesc.ArraySize        = 1;
+   textureDesc.Format           = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
    textureDesc.SampleDesc.Count = 1;
-   textureDesc.Usage = D3D11_USAGE_IMMUTABLE;
-   textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+   textureDesc.Usage            = D3D11_USAGE_IMMUTABLE;
+   textureDesc.BindFlags        = D3D11_BIND_SHADER_RESOURCE;
 
    D3D11_SUBRESOURCE_DATA textureData = {};
-   textureData.pSysMem = TextureData;
+   textureData.pSysMem     = TextureData;
    textureData.SysMemPitch = TEXTURE_WIDTH * sizeof(UINT); // 4 bytes per pixel
-
    ID3D11Texture2D* texture;
    device->CreateTexture2D(&textureDesc, &textureData, &texture);
    ID3D11ShaderResourceView* textureView;
@@ -245,7 +248,6 @@ int main(int argc, char** args)
    float3 modelScale = { 1.0f, 1.0f, 1.0f };
    float3 modelTranslation = { 0.0f, 0.0f, 4.0f };
 
-
    const uint8_t* keys = SDL_GetKeyboardState(nullptr);
    while (true)
    {
@@ -255,13 +257,36 @@ int main(int argc, char** args)
          break;
          OutputDebugString(L"escape pressed\r");
       }
-         
 
-      matrix rotateX = { 1, 0, 0, 0, 0, static_cast<float>(cos(modelRotation.x)), -static_cast<float>(sin(modelRotation.x)), 0, 0, static_cast<float>(sin(modelRotation.x)), static_cast<float>(cos(modelRotation.x)), 0, 0, 0, 0, 1 };
-      matrix rotateY = { static_cast<float>(cos(modelRotation.y)), 0, static_cast<float>(sin(modelRotation.y)), 0, 0, 1, 0, 0, -static_cast<float>(sin(modelRotation.y)), 0, static_cast<float>(cos(modelRotation.y)), 0, 0, 0, 0, 1 };
-      matrix rotateZ = { static_cast<float>(cos(modelRotation.z)), -static_cast<float>(sin(modelRotation.z)), 0, 0, static_cast<float>(sin(modelRotation.z)), static_cast<float>(cos(modelRotation.z)), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
-      matrix scale = { modelScale.x, 0, 0, 0, 0, modelScale.y, 0, 0, 0, 0, modelScale.z, 0, 0, 0, 0, 1 };
-      matrix translate = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, modelTranslation.x, modelTranslation.y, modelTranslation.z, 1 };
+      //matrix rotateX = { 1, 0, 0, 0,
+      //                   0, static_cast<float>(cos(modelRotation.x)), -static_cast<float>(sin(modelRotation.x)), 0,
+      //                   0, static_cast<float>(sin(modelRotation.x)),  static_cast<float>(cos(modelRotation.x)), 0, 
+      //                   0, 0, 0, 1 };
+      //matrix rotateY = { static_cast<float>(cos(modelRotation.y)), 0, static_cast<float>(sin(modelRotation.y)), 0, 0,
+      //                   1, 0, 0, -static_cast<float>(sin(modelRotation.y)),
+      //                   0, static_cast<float>(cos(modelRotation.y)), 0,
+      //                   0, 0, 0, 1 };
+      //matrix rotateZ = { static_cast<float>(cos(modelRotation.z)), -static_cast<float>(sin(modelRotation.z)), 0, 0,
+      //                   static_cast<float>(sin(modelRotation.z)),  static_cast<float>(cos(modelRotation.z)), 0, 0,
+      //                   0, 0, 1, 0,
+      //                   0, 0, 0, 1 };
+
+      // matrix scale = { modelScale.x, 0, 0, 0,
+      //            0, modelScale.y, 0, 0,
+      //            0, 0, modelScale.z, 0,
+      //            0, 0, 0, 1 };
+      // 
+      //matrix translate = { 1, 0, 0, 0,  
+      //                     0, 1, 0, 0,
+      //                     0, 0, 1, 0,
+      //                     modelTranslation.x, modelTranslation.y, modelTranslation.z, 1 };
+
+      glm::mat4 model(1.0f);
+      auto rotateX = glm::rotate(model, modelRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+      auto rotateY = glm::rotate(model, modelRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+      auto rotateZ = glm::rotate(model, modelRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+      auto scale = glm::scale(model, glm::vec3(modelScale.x, modelScale.y, modelScale.z));
+      auto translate = glm::translate(model, glm::vec3(modelTranslation.x, modelTranslation.y, modelTranslation.z));
 
       modelRotation.x += 0.005f;
       modelRotation.y += 0.009f;
@@ -270,8 +295,14 @@ int main(int argc, char** args)
       D3D11_MAPPED_SUBRESOURCE mappedSubresource;
       deviceContext->Map(constantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedSubresource);
       Constants* constants = reinterpret_cast<Constants*>(mappedSubresource.pData);
-      constants->Transform = rotateX * rotateY * rotateZ * scale * translate;
-      constants->Projection = { 2 * n / w, 0, 0, 0, 0, 2 * n / h, 0, 0, 0, 0, f / (f - n), 1, 0, 0, n * f / (n - f), 0 };
+      // constants->Transform =  rotateX * rotateY * rotateZ * scale * translate;
+      constants->Transform = translate * rotateX * rotateY * rotateZ * scale;
+      //constants->Projection = { 2 * n / w, 0,         0,               0,
+      //                          0,         2 * n / h, 0,               0,
+      //                          0,         0,         f / (f - n),     1,
+      //                          0,         0,         n * f / (n - f), 0 };
+      //
+      constants->Projection = glm::perspectiveLH(glm::radians(60.f), w, n, f);
       constants->LightVector = { 1.0f, -1.0f, 1.0f };
       deviceContext->Unmap(constantBuffer, 0);
 
@@ -298,33 +329,9 @@ int main(int argc, char** args)
       deviceContext->OMSetDepthStencilState(depthStencilState, 0);
       deviceContext->OMSetBlendState(nullptr, nullptr, 0xffffffff); // use default blend mode (i.e. disable)
 
-
       deviceContext->DrawIndexed(ARRAYSIZE(IndexData), 0, 0);
 
       swapChain->Present(1, 0);
    }
    return 0;
-}
-
-matrix operator*(const matrix& m1, const matrix& m2)
-{
-	return
-	{
-		 m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0],
-		 m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1],
-		 m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2],
-		 m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3],
-		 m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0],
-		 m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1],
-		 m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2],
-		 m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3],
-		 m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0],
-		 m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1],
-		 m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2],
-		 m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3],
-		 m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0],
-		 m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1],
-		 m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2],
-		 m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3],
-	};
 }
